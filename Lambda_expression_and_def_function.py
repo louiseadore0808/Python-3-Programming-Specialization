da248d4f635b6ae7ba76de991abb8d84876341c1
{louiseadore0808} <louiseadore0808@gmail.com>
2021-07-31 22:39:36 +0800

    Lambda expression and optional key parametere

--- Lambda_expression_and_def_function.py
+++ Lambda_expression_and_def_function.py
@@ -22,3 +22,10 @@ def last_char(s):
     return s[-1]
 # can also write in
 last_char = (lambda s: s[-1])
+
+# Once again, sort the list nums based on the last digit of each number from highest to lowest. 
+# However, now you should do so by writing a lambda function. Save the new list as nums_sorted_lambda.
+
+nums = ['1450', '33', '871', '19', '14378', '32', '1005', '44', '8907', '16']
+
+nums_sorted_lambda = sorted(nums, reverse=True, key=lambda x: x[-1])
