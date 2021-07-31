782c13db908f812108c7f87109a9ddd67d5f10d1
{louiseadore0808} <louiseadore0808@gmail.com>
2021-07-31 22:39:52 +0800

    optional key parameter

--- Optional_Parameters指定預設參數值.py
+++ Optional_Parameters指定預設參數值.py
@@ -38,6 +38,48 @@ initial = 10  # default values are only evaluated when we declare the function.
 # z is going to have the value seven by default.
 f(2) #initial in def不變
 
-# Write a function called str_mult that takes in a required string parameter and an optional integer parameter. The default value for the integer parameter should be 3. The function should return the string multiplied by the integer parameter.
+# Write a function called str_mult that takes in a required string parameter and an optional integer
+#  parameter. The default value for the integer parameter should be 3. The function should return the
+#  string multiplied by the integer parameter.
 def str_mult(a, b=3):
     return a*b
+
+# When we write key=some_function in the function invocation, the word key is there because
+# it is the name of the parameter, specified in the definition of the sort function,
+# not because we are using keyword-based parameter passing.
+
+# You will be sorting the following list by each element’s second letter, a to z. 
+# Create a function to use when sorting, called second_let. It will take a string as input 
+# and return the second letter of that string. Then sort the list, create a variable called 
+# sorted_by_second_let and assign the sorted list to it. Do not use lambda.
+
+# It depends on how sorted works when it has a key parameter that takes a function as value:
+ex_lst = ['hi', 'how are you', 'bye', 'apple', 'zebra', 'dance']
+
+def second_let(x):
+    return x[1]
+
+sorted_by_second_let = sorted(ex_lst, key=second_let)
+# The sorted function loops through each item of the list when it is used like this. 
+# That is why there is no need for a for loop.
+
+# Below, we have provided a list of strings called nums. Write a function called last_char that 
+# takes a string as input, and returns only its last character. Use this function to sort the list 
+# nums by the last digit of each number, from highest to lowest, and save this as a new list 
+# called nums_sorted.
+
+
+nums = ['1450', '33', '871', '19', '14378', '32', '1005', '44', '8907', '16']
+
+def last_char(s):
+    return s[-1]
+
+nums_sorted = sorted(nums, reverse=True, key=last_char)
+print(nums_sorted)
+
+# Once again, sort the list nums based on the last digit of each number from highest to lowest.
+# However, now you should do so by writing a lambda function. Save the new list as nums_sorted_lambda.
+
+nums = ['1450', '33', '871', '19', '14378', '32', '1005', '44', '8907', '16']
+
+nums_sorted_lambda = sorted(nums, reverse=True, key=lambda x: x[-1])
